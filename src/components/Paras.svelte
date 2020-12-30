1cb7580eed516d3d7ecb6229208ef02dc89a4ed3
Aaron Rosen <aaronorosen@gmail.com>
2020-12-29 20:32:49 -0500

    fix up

--- src/components/Paras.svelte
+++ src/components/Paras.svelte
@@ -52,24 +52,21 @@
 <div class="wrapper">
   <span>
     <p>
-      Dream Potential is an accelerator program and technology collective. Some
-      other information goes here that mentions something really cool.
+      Dream Potential is an accelerator program and technology collective.
     </p>
     <div><a href="#connect">Apply Accelerator Program</a></div>
   </span>
 
   <span>
     <p>
-      Bring your Vision we manifest your projects. Some other information goes
-      here that mentions something really cool.
+      Bring your Vision we manifest your projects.
     </p>
     <div><a href="#connect">Build Vision</a></div>
   </span>
 
   <span>
     <p>
-      Not sure what how to enhance your organization or business. Some other
-      information goes here that mentions something really cool.
+      Not sure what how to enhance your organization or business.
     </p>
     <div><a href="#connect">Get in Touch</a></div>
   </span>
